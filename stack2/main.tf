147c93fc3cee8d659f5bdfafee93d4bb667a9ecc
Ansar Rezaei <ansar.rezaei@gmail.com>
2025-11-04 19:08:34 +0100

    Update main.tf with Aurora configuration details including ARN, endpoint, secret ARN, and S3 bucket ARN.

--- stack2/main.tf
+++ stack2/main.tf
@@ -8,15 +8,15 @@ module "bedrock_kb" {
   knowledge_base_name        = "my-bedrock-kb"
   knowledge_base_description = "Knowledge base connected to Aurora Serverless database"
 
-  aurora_arn        = "" #TODO Update with output from stack1
+  aurora_arn        = "arn:aws:rds:us-west-2:454226392634:cluster:my-aurora-serverless" #TODO Update with output from stack1
   aurora_db_name    = "myapp"
-  aurora_endpoint   = "" # TODO Update with output from stack1
+  aurora_endpoint   = "my-aurora-serverless.cluster-cxqcfyjwmfdi.us-west-2.rds.amazonaws.com" # TODO Update with output from stack1
   aurora_table_name = "bedrock_integration.bedrock_kb"
   aurora_primary_key_field = "id"
   aurora_metadata_field = "metadata"
   aurora_text_field = "chunks"
   aurora_vector_field = "embedding"
   aurora_username   = "dbadmin"
-  aurora_secret_arn = "" #TODO Update with output from stack1
-  s3_bucket_arn = "" #TODO Update with output from stack1
+  aurora_secret_arn = "arn:aws:secretsmanager:us-west-2:454226392634:secret:my-aurora-serverless-fkw4Ul" #TODO Update with output from stack1
+  s3_bucket_arn = "arn:aws:s3:::bedrock-kb-454226392634" #TODO Update with output from stack1
 }
